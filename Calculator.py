82eab310609235c5526deb813bbc8a3da17fa8ba
AnumFatima192 <shabbircitypk2@gmail.com>
2026-09-22 17:39:12 +0500

    Fix calculator error

--- Calculator.py
+++ Calculator.py
@@ -1,6 +1,6 @@
 num1=float(input("Enter first number: "))
 num2=float(input("Enter second number: "))
-print(Addition: ",num1+num2)
+print("Addition: ",num1+num2)
 print("Substraction: ",num1-num2)
 ptint("Multiplication: ",num1*num2)
 if num2 !=0:
