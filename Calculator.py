9a6ef52ae07c1f8b2707243fc7eed652422f5a37
AnumFatima192 <shabbircitypk2@gmail.com>
2026-09-22 17:34:16 +0500

    Implement basic calculator functions
    Fix print statement for addition and correct typo in multiplication.

---
+++ Calculator.py
@@ -0,0 +1,9 @@
+num1=float(input("Enter first number: "))
+num2=float(input("Enter second number: "))
+print(Addition: ",num1+num2)
+print("Substraction: ",num1-num2)
+ptint("Multiplication: ",num1*num2)
+if num2 !=0:
+  print("Division: ",num1/num2)
+else:
+  print("Division is not possible because the second number is zero(0)")
